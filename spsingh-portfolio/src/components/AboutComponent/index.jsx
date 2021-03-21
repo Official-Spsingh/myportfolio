26ddf6b0617f9d9336fef0de61127eeffd9cc550
Shubham Pratap Singh <SPSingh@bpl.netlink.com>
2021-03-21 12:13:36 +0530

    changes in about

--- spsingh-portfolio/src/components/AboutComponent/index.jsx
+++ spsingh-portfolio/src/components/AboutComponent/index.jsx
@@ -6,10 +6,13 @@ const AboutComponent = () => {
     return (
         <div className='about'>
             <div className="about__img">
-                <img src={aboutimg} alt='spsingh'/>
+                <img src={aboutimg} alt='spsingh' />
             </div>
             <div className="about__para">
-                I am a person who loves coding and eventually shapes and compares everything with coding. I like to know about new technologies and I feel this is the way of making oneself sustainable. I like writing blogs and like using new tools which makes life easier.
+                Full stack engineer having grasp knowledge and experience on MERN stack.
+                I am comfortable with a lot of web technologies & frameworks and would love to work in a very dynamic & aspirational environment.
+                I like to know about new technologies and I feel this is the way of making oneself sustainable. I like writing blogs and like using new tools which makes life easier.
+
                 <a href={SpPdf} download='Shubham Pratap Singh Resume'>
                     <DownloadOutlined />Resume</a>
             </div>
