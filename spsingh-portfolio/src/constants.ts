444cec251b0e6a6ca7c1e76188e7c2ad0c1de45c
shubham pratap singh <shubhampratp.singh@gmail.com>
2026-02-22 16:38:24 +0530

    chanhges added

--- spsingh-portfolio/src/constants.ts
+++ spsingh-portfolio/src/constants.ts
@@ -1,10 +1,10 @@
 
 import { Experience, Education, Project, SkillCategory, ResumeData } from './types';
-import pImage from './media/aboutimg2.jpeg';
+import pImage from './media/aboutimg2.webp';
 import SpPdf from './media/sps.pdf'
 import SimillimumPro from './media/SimillimumPro.png'
 import Iterion from './media/Iterion.png'
-import lumenore from './media/lumenore.svg'
+import lumenore from './media/lumenore.webp'
 
 export const SOCIAL_LINKS = {
   linkedin: "https://www.linkedin.com/in/shubhampratapsingh/",
